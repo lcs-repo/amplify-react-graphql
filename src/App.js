cf6fadd5c33e602fa2540acc9aa77eaca1ab44e3
Lucas <133768686+lcs-repo@users.noreply.github.com>
2024-07-19 19:49:59 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div classname="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          DO NOT EDIT! <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Hello from V2</h1>
+      </header> 
     </div>
   );
 }
